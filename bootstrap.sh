b459071c0e4cf57ced959658055df058a157e4f4
ryanshepps <sheppardryan@rogers.com>
2022-09-21 11:39:43 -0400

    authenticate with gcloud

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,12 +13,21 @@ echo_usage() {
   echo "Usage: $0 PROJECT_ID"
 }
 
+authenticate() {
+  echo "Log into an account that has storage.buckets.list permissions on your Google Cloud Project."
+  gcloud auth login
+}
+
 main() {
   if ! verify_params $@
   then
     echo_usage
     exit 1
   fi 
+
+  local project_name="${1}"
+
+  authenticate
 }
 
 main "$@"
